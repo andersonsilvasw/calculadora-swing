3a28867d461bd9651637b394470ec937541ca4c5
andersonsilvasw <andersonsilvaedif@hotmail.com>
2024-01-31 11:24:47 -0300

    Sessão 13 - Aula 20 Componente Teclado #02

--- src/br/com/cod3r/calc/visao/Teclado.java
+++ src/br/com/cod3r/calc/visao/Teclado.java
@@ -15,10 +15,14 @@ public class Teclado extends JPanel {
 
         setLayout(layout);
 
+        c.weightx = 1;
+        c.weighty = 1;
+        c.fill = GridBagConstraints.BOTH;
+
         //LINHA 1
+        c.gridwidth = 3;
         adicionarBotao("AC", COR_CINZA_ESCURO, c, 0, 0);
-        adicionarBotao("+/-", COR_CINZA_ESCURO, c, 1, 0);
-        adicionarBotao("%", COR_CINZA_ESCURO, c, 2, 0);
+        c.gridwidth = 1;
         adicionarBotao("/", COR_LARANJA, c, 3, 0);
 
         //LINHA 2
@@ -40,8 +44,9 @@ public class Teclado extends JPanel {
         adicionarBotao("+", COR_LARANJA, c, 3, 3);
 
         //LINHA 5
+        c.gridwidth = 2;
         adicionarBotao("0", COR_CINZA_CLARO, c, 0, 4);
-        adicionarBotao("0", COR_CINZA_CLARO, c, 1, 4);
+        c.gridwidth = 1;
         adicionarBotao(",", COR_CINZA_CLARO, c, 2, 4);
         adicionarBotao("=", COR_LARANJA, c, 3, 4);
     }
